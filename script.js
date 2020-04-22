a1453b34bff2d1cf7a844378eab396527ed804fb
deepak <deepak.hk6@gmail.com>
2020-04-22 20:34:16 +0530

    commit

---
+++ script.js
@@ -0,0 +1,123 @@
+/*/var firstName ='deepak' ;
+console.log(firstName);
+
+
+
+var lastName ='ashok';
+console.log(lastName);
+var fullage =true;
+console.log(fullage);
+
+
+var job;
+console.log(job);
+job ='TEACHER';
+console.log(job);
+
+var $3years =3;
+var johnmark ='john and mark';
+
+    
+
+
+var firstName ='john';
+var age =29;
+console.log(firstName +' '+ age);
+isMarried =false;
+
+
+console.log(firstName+' is a'+age+ 'yearold'+job+'Is a married'+isMarried);
+
+age ='twenti eight';
+job ='driver';
+
+alert(firstName +'is a'+age+'year old'+job+'id he married'+isMarried);
+
+var lastName =prompt('what is hislast name');
+console.log(firstName +'' +lastName);
+
+
+var year,yearjohn,yearMark;
+now =2018;
+agejohn=28;
+ageMark=33;
+
+var year=2018;
+var yearjohn =2018 -28;
+var yashMark =2018 -33;
+console.log(yearjohn);
+console.log(now +2);
+console.log(now*2);
+console.log(now/10);
+
+var johnOlder =agejohn >ageMark;
+console.log(johnOlder);
+
+
+console.log(typeof johnOlder);*/
+
+
+
+//type of operator
+
+/*console.log(typeof johnOlder);
+console.log(typeof agejohn);
+console.log(typeof 'mark is older thn john');
+var x;
+console.log(typeof x);*/
+/*var now=2018;
+var yearJohn=1989;
+var fullAge=18;
+//
+var isFullAge=now -yearJohn >=fullAge;
+console.log(isFullAge); 
+
+var average;
+var ageJohn =now-yearJohn;
+var ageMark =35;
+var aeverage =(ageJohn + ageMark) / 2;
+console.log(average);
+
+var x,y;
+x=y=(3+5)*4-6;
+console.log(x,y);
+x =x*2;
+console.log(x);
+x =x+1;
+x +=1;
+x++
+console.log(x);*/
+var firstname ='john';
+var civilStatus ='single';
+
+if(civilStatus=='married')
+{
+    console.log(firstname +'is married');
+  }  else{
+        console.log(firstname +'will hopefully mary soon:');
+    }
+
+var isMarried =false;
+if(isMarried==true)
+{
+    console.log(firstname +'is married');
+}
+    else{
+        console.log(firstname + 'will hopefully marry soon');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
